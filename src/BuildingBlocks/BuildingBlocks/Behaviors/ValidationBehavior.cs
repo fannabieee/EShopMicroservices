5f0bba9e39115d1bbe6cc16109de5bf649b4469c
fanabie <namplfptu.dev@gmail.com>
2026-08-12 20:44:59 +0700

    Add FluentValidation pipeline behavior for CQRS commands
    Implemented ValidationBehavior using MediatR's IPipelineBehavior to validate CQRS commands with FluentValidation before handling. Integrated FluentValidation via dependency injection by adding the relevant NuGet package to BuildingBlocks.csproj and Catalog.API.csproj. Validation failures now throw a ValidationException.

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,27 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using MediatR;
+
+namespace BuildingBlocks.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validator)
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : ICommand<TRequest>
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationResults = await Task.WhenAll(validator.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+            var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+            if (failures.Any())
+            {
+                throw new ValidationException(failures);
+            }
+
+            return await next();
+        }
+    }
+}
